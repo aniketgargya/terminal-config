e1d706409e5a62823f575128819a2d741aa3f282
Aniket Gargya <gargya.aniket@gmail.com>
2021-04-21 20:21:40 -0500

    kill git pull in 5 seconds

--- init-terminal-config.sh
+++ init-terminal-config.sh
@@ -1,5 +1,5 @@
 echo "~~~~~~~~~~~~~~~~~~~ Starting Terminal Config ~~~~~~~~~~~~~~~~~~~"
-timeout 10s git -C ~/terminal-config/ pull
+timeout 5s git -C ~/terminal-config/ pull
 source ~/terminal-config/terminal-config.sh
 echo "~~~~~~~~~~~~~~~~~~~~ Ending Terminal Config ~~~~~~~~~~~~~~~~~~~~"
 
